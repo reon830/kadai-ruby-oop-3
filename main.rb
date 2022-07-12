96a0e53d4b88617927bcda8e20cc89cd40ebc6b3
reon <reon08830@gmail.com>
2022-07-12 21:26:11 +0000

    kadai

---
+++ main.rb
@@ -0,0 +1,14 @@
+require './human'
+
+# インスタンスの作成
+tanaka = Human.new("田中太郎",25,"電車")
+suzuki = Human.new("鈴木次郎",30,"野球")
+sato   = Human.new("佐藤花子",20,"映画")
+
+# メソッドを実行
+tanaka.say
+tanaka.think
+suzuki.say
+suzuki.think
+sato.say
+sato.think
